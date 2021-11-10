fc46d4c330d58ae236e51223604afe5194cceeff
tlik <iwikotan8@gmail.com>
2021-11-10 12:43:39 +0100

    9

--- clinique/donnees.py
+++ clinique/donnees.py
@@ -5,7 +5,7 @@ class info:
               {'id':3,'noms':'ADAM','prenoms':'smith','age':'1ans','sexe':'M','statut':'particulier'},
         
     ]
-
+    #il faut souvent ajouter @classmethod pour ajouter une methode de class et qui prend cls 
     @classmethod
     def all(cls):
         return cls.contenue
